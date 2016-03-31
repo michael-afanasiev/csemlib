fad27c655849f37ead15504484fe900c5b25dc16
michael_afanasiev <mafanasiev@gmail.com>
2016-03-31 23:16:41 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=['click'],
     entry_points='''
     [console_scripts]
     csem=csemlib.csemlib:cli
-    ''', requires=['click']
+    '''
 )
