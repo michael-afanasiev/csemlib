d0228ad9cddb18c34fab91787e667290bf64eae6
Dirk-Philip <dirkphilip@gmail.com>
2016-11-20 14:09:22 +0100

    Expanded the s20rts.eval method such that also deeper layers can be visualised

--- csemlib/models/s20rts.py
+++ csemlib/models/s20rts.py
@@ -39,16 +39,37 @@ class S20rts(Model):
         pass
 
     def eval(self, x, y, z, param):
-
+        idx = find_layer_idx(z)
         val = np.zeros_like(x)
         for n in range(20):
             imag, real = 1, 0
             for m in range(2 * n + 1):
                 if m == 0 or (m % 2):
-                    val += self.l0[n][m] * np.real(sph_harm(real, n, y, x))
+                    #val += self.l0[n][m] * np.real(sph_harm(real, n, y, x))
+                    val += getattr(self, 'l%d' % idx)[n][m] * np.real(sph_harm(real, n, y, x))
                     real += 1
                 else:
-                    val += self.l0[n][m] * np.imag(sph_harm(imag, n, y, x))
+                    val += getattr(self, 'l%d' % idx)[n][m] * np.imag(sph_harm(imag, n, y, x))
                     imag += 1
 
         return val
+
+def find_layer_idx(z):
+    """
+    Return the index of the s20rts layer for the requested depth
+    :param z: distance from core in km
+    :return layer index:
+    """
+
+    s20_layers = [6346.63, 6296.63, 6241.64, 6181.14, 6114.57, 6041.34, 5960.79, 5872.18, 5774.69, 5667.44, 5549.46,
+              5419.68,5276.89, 5119.82, 4947.02, 4756.93, 4547.81, 4317.74, 4064.66, 3786.25, 3479.96]
+
+    if z < s20_layers[-1]:
+        raise ValueError('Requested layer out of bounds for s20rts')
+
+    idx, nearest_layer_depth = min(enumerate(s20_layers), key=lambda x: abs(x[1] - z))
+
+    if z < nearest_layer_depth
+        idx += 1
+
+    return idx
